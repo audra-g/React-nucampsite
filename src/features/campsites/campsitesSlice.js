6d862bc789579ea08967c7bfde4c20b59a6fbdbe
Audra Geary <audrageary@gmail.com>
2023-04-07 23:38:37 -0700

    Redux campsitesSlice

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,16 +1,28 @@
 //notice name of file: lowercase C for first letter > this will be the case for ny file that will NOT export a React component.
 //notice file is also plural
 
-import { CAMPSITES } from '../../app/shared/CAMPSITES'; 
+import { createSlice } from "@reduxjs/toolkit";
+import { CAMPSITES } from "../../app/shared/CAMPSITES";
+
+const initialState = {
+  campsitesArray: CAMPSITES,
+};
+
+const campsitesSlice = createSlice({
+  name: "campsites",
+  initialState,
+});
+
+export const campsitesReducer = campsitesSlice.reducer;
 
 export const selectAllCampsites = () => {
-    return CAMPSITES;
-}; 
+  return CAMPSITES;
+};
 
 export const selectCampsiteById = (id) => {
-    return CAMPSITES.find((campsite) => campsite.id === parseInt(id)); 
+  return CAMPSITES.find((campsite) => campsite.id === parseInt(id));
 };
 
 export const selectFeaturedCampsite = () => {
-    return CAMPSITES.find((campsite) => campsite.featured);
+  return CAMPSITES.find((campsite) => campsite.featured);
 };
